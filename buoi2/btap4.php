9dc1cf4a7ae501be917d44397a4acc0af0a2e04b
dat20004 <nvdat22082004@gmail.com>
2024-08-22 23:04:51 +0700

    c

--- buoi2/btap4.php
+++ buoi2/btap4.php
@@ -4,23 +4,19 @@ function sumArray($arr) {
     return array_sum($arr);
 }
 
-// Hàm tìm giá trị lớn nhất trong mảng
 function maxArray($arr) {
     return max($arr);
 }
 
-// Hàm tìm giá trị nhỏ nhất trong mảng
 function minArray($arr) {
     return min($arr);
 }
 
-// Hàm sắp xếp mảng theo thứ tự tăng dần
 function sortArrayAscending($arr) {
     sort($arr);
     return $arr;
 }
 
-// Hàm sắp xếp mảng theo thứ tự giảm dần
 function searchInArray($arr, $value) {
     return in_array($value, $arr);
 }
